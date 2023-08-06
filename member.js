d9f6f752e86980db9dbe1a336604eca757ab3661
Michael Murphy <michael.t.murphy@gmail.com>
2023-08-06 16:45:49 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St'
+    };
+
+    return {
+        name: member.name,
+        age: member.age,
+        address: member.address
+    };
+}
